341851f83fbd70cd4dcbdcd7cfa344c7081955f7
Camilo Andres Riveros Rey <camilosk8.rey@gmail.com>
2022-03-15 12:00:31 -0500

    se agrega el proyecto

---
+++ src/js/app.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    navegacionFija();
+    crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function() {
+        console.log(sobreFestival.getBoundingClientRect());
+        if (sobreFestival.getBoundingClientRect().top <= 0) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
+        <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
+        <img width="200" height="300px" src="build/img/thumb/${i}.webp" alt="imagen galeria" loading="lazy">
+        `;
+
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="imagen/avif">
+        <source srcset="build/img/grande/${id}.webp" type="imagen/webp">
+        <img width="200" height="300px" src="build/img/grande/${id}.webp" alt="imagen galeria" loading="lazy">
+        `;
+    //creando el overlay con la img 
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+
+    //boton para cerrar el modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+    //añadirlo al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+}
